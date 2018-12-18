8b4524d3db02c23966a812abb096936a3a82148d
amshelhack3r <amshelhack3r@gmail.com>
2018-12-18 21:11:43 +0300

    refractor based on pep-8 standards

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import  Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify
 import requests
 from app.otaku import Otaku
 import json as myjson
@@ -8,15 +8,17 @@ base_url = "https://api.jikan.moe/v3"
 manga = Otaku("manga")
 anime = Otaku("anime")
 
+
 @app.route("/")
 def main():
     return render_template("index.html")
+
 
 @app.route("/anime/random")
 def random_anime():
     response = requests.get(base_url+anime.get_random_media())
     if response.status_code == 200:
-        return render_template('random.html', data = response.json())
+        return render_template('random.html', data=response.json())
     else:
         return random_anime()
 
@@ -26,7 +28,7 @@ def random_manga():
     response = requests.get(base_url+manga.get_random_media())
     if response.status_code == 200:
         print(response.json())
-        return render_template('random.html', data = response.json())
+        return render_template('random.html', data=response.json())
     else:
         return random_manga()
 
@@ -49,6 +51,7 @@ def get_anime_by_season(season, year):
     response = requests.get(base_url + Otaku.get_anime_by_season(year, season))
     return jsonify(response.json())
 
+
 @app.route("/anime/genre/<genre>")
 def get_anime_by_genre(genre):
     response = requests.get(base_url+anime.get_media_by_genre(genre))
@@ -59,6 +62,7 @@ def get_anime_by_genre(genre):
 def get_manga_by_genre(genre):
     response = requests.get(base_url+manga.get_media_by_genre(genre))
     return jsonify(response.json())
+
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
