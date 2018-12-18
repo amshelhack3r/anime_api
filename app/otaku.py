a0376ac1ab6e8432f5c32183ecb998ff172bc247
amshelhack3r <amshelhack3r@gmail.com>
2018-12-18 21:09:32 +0300

    add genre ids

--- app/otaku.py
+++ app/otaku.py
@@ -5,13 +5,13 @@ similar the difference is the type of content.
 
 '''
 
-#bring in the random library to generate a random integer
+# bring in the random library to generate a random integer
 from random import randint
 
 
 class Otaku:
     def __init__(self, media):
-        self.media = media;
+        self.media = media
         self.endpoint = '/'+media
         pass
 
@@ -28,46 +28,45 @@ class Otaku:
     def get_anime_by_season(year, season):
         return '/season/'+year+'/'+season
 
-
     def get_media_by_genre(self, genre):
         genres = {
-            "action":1,
-            "adventure":2,
-            "cars":3,
-            "comedy":4,
-            "dementia":5,
-            "demons":6,
-            "mystery":7,
-            "drama":8,
-            "ecchi":9,
-            "fantasy":10,
-            "game":11,
-            "harem":35,
-            "hentai":12,
-            "historical":13,
-            "horror":14,
-            "josei":43,
-            "kids":15,
-            "magic":16,
-            "martial_arts":17,
-            "mecha":18,
-            "military":38,
-            "music":19,
-            "parody":20,
-            "police":39,
-            "psychological":40,
-            "shoujo":25,
-            "shoujo ai":26,
-            "shounen":27,
-            "shounen ai":28,
-            "slice of life":36,
-            "space":29,
-            "sports":30,
-            "superpower":31,
-            "supernatural":37,
-            "thriller":41,
-            "vampire":32,
-            "yuri":34,
-            "yaoi":33
+            "action": 1,
+            "adventure": 2,
+            "cars": 3,
+            "comedy": 4,
+            "dementia": 5,
+            "demons": 6,
+            "mystery": 7,
+            "drama": 8,
+            "ecchi": 9,
+            "fantasy": 10,
+            "game": 11,
+            "harem": 35,
+            "hentai": 12,
+            "historical": 13,
+            "horror": 14,
+            "josei": 43,
+            "kids": 15,
+            "magic": 16,
+            "martial_arts": 17,
+            "mecha": 18,
+            "military": 38,
+            "music": 19,
+            "parody": 20,
+            "police": 39,
+            "psychological": 40,
+            "shoujo": 25,
+            "shoujo ai": 26,
+            "shounen": 27,
+            "shounen ai": 28,
+            "slice of life": 36,
+            "space": 29,
+            "sports": 30,
+            "superpower": 31,
+            "supernatural": 37,
+            "thriller": 41,
+            "vampire": 32,
+            "yuri": 34,
+            "yaoi": 33
         }
         return '/genre/'+self.media+'/'+str(genres[genre])
