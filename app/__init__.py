3ea126b76b1c668079f19640e0a45722dcc3d70f
amshelhack3r <amshelhack3r@gmail.com>
2018-12-18 21:09:44 +0300

    add todos

---
+++ app/__init__.py
@@ -0,0 +1,6 @@
+#TODO parse the json data for a manga and anime item and get essential info
+#todo create a seasons model to retrieve data of a particular season
+#todo get media by genre
+#todo get the top anime/manga based on the myanimelist website
+#todo add search functionality(ajax) to the application
+#todo create html/css markup for displaying the data
