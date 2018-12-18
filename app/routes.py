0c5fd5be70a06a157d60e8a14e4a93599201f0c6
amshelhack3r <amshelhack3r@gmail.com>
2018-12-18 22:39:33 +0300

    use flask restful to implement endpoints

--- app/routes.py
+++ app/routes.py
@@ -1,67 +1,57 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, make_response
+from flask_restful import Resource, Api
 import requests
 from app.otaku import Otaku
-import json as myjson
 
 app = Flask(__name__, template_folder='views')
+# initialize flask restful
+api = Api(app)
+
+# get our models
 base_url = "https://api.jikan.moe/v3"
-manga = Otaku("manga")
-anime = Otaku("anime")
 
 
-@app.route("/")
-def main():
-    return render_template("index.html")
+class Default(Resource):
+    def get(self):
+        return make_response(render_template('index.html'),200)
 
 
-@app.route("/anime/random")
-def random_anime():
-    response = requests.get(base_url+anime.get_random_media())
-    if response.status_code == 200:
-        return render_template('random.html', data=response.json())
-    else:
-        return random_anime()
+class SearchMedia(Resource):
+    def get(self, title, type):
+        media = Otaku(type)
+        response = requests.get(base_url+media.get_media_by_title(title))
+        return jsonify(response.json())
 
 
-@app.route("/manga/random")
-def random_manga():
-    response = requests.get(base_url+manga.get_random_media())
-    if response.status_code == 200:
-        print(response.json())
-        return render_template('random.html', data=response.json())
-    else:
-        return random_manga()
+class RandomMedia(Resource):
+    def get(self, type):
+        media = Otaku(type)
+        response = requests.get(base_url+media.get_random_media())
+        if(response.status_code == 200):
+            return make_response(render_template('random.html', data=response.json()),200)
+        else:
+            return self.get(type)
 
 
-@app.route("/anime/<title>")
-def search_anime(title):
-    print(base_url+anime.get_media_by_title(title))
-    response = requests.get(base_url+anime.get_media_by_title(title))
-    return jsonify(response.json())
+class Season(Resource):
+    def get(self, year, season):
+        response = requests.get(base_url + Otaku.get_anime_by_season(year, season))
+        return jsonify(response.json())
 
 
-@app.route("/manga/<title>")
-def search_manga(title):
-    response = requests.get(base_url+manga.get_media_by_title(title))
-    return jsonify(response.json())
+class MediaGenre(Resource):
+    def get(self, type, genre):
+        media = Otaku(type)
+        response = requests.get(base_url+media.get_media_by_genre(genre))
+        return jsonify(response.json())
 
 
-@app.route("/season/<season>/<year>")
-def get_anime_by_season(season, year):
-    response = requests.get(base_url + Otaku.get_anime_by_season(year, season))
-    return jsonify(response.json())
-
-
-@app.route("/anime/genre/<genre>")
-def get_anime_by_genre(genre):
-    response = requests.get(base_url+anime.get_media_by_genre(genre))
-    return jsonify(response.json())
-
-
-@app.route("/manga/genre/<genre>")
-def get_manga_by_genre(genre):
-    response = requests.get(base_url+manga.get_media_by_genre(genre))
-    return jsonify(response.json())
+# add endpoints to resources
+api.add_resource(Default, '/')
+api.add_resource(SearchMedia, '/<string:type>/<string:title>')
+api.add_resource(RandomMedia, '/random/<string:type>')
+api.add_resource(Season, '/season/<string:year>/<string:season>')
+api.add_resource(MediaGenre, '/<string:type>/genre/<string:genre>')
 
 
 if __name__ == '__main__':
